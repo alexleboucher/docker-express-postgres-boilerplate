1db8e0635b4ef1bc45575110d2488f2b50805a0d
Alex <dev.alexisleboucher@gmail.com>
2023-03-18 18:29:16 +0100

    Add protection middlewares (#5)
    * Added auth middlewares
    
    * Updated tests and added tsconfig for build

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import AuthController from '../controllers/auth';
+import { isAuthenticated, isUnauthenticated } from '../middlewares/auth';
 
 const router = express.Router();
 
-router.route('/login').post(AuthController.login);
-router.route('/logout').post(AuthController.logout);
+router.route('/login').post(isUnauthenticated, AuthController.login);
+router.route('/logout').post(isAuthenticated, AuthController.logout);
 router.route('/authenticated').get(AuthController.authenticated);
 
 export default router;
