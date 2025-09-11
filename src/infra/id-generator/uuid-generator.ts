7a7e2fec7fa8d3b9fc7f4aea85bb359b82c043d6
Alex <dev.alexisleboucher@gmail.com>
2025-09-11 17:45:27 +0200

    remove uuid package to use built-in crypto (#59)

--- src/infra/id-generator/uuid-generator.ts
+++ src/infra/id-generator/uuid-generator.ts
@@ -1,11 +1,11 @@
+import { randomUUID } from 'crypto';
 import { injectable } from 'inversify';
-import { v4 as uuidv4 } from 'uuid';
 
 import type { IIDGenerator } from '@/core/id/id-generator.interface';
 
 @injectable()
 export class UUIDGenerator implements IIDGenerator {
   generate() {
-    return uuidv4();
+    return randomUUID();
   }
 }
